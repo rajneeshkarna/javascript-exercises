17406a7abd92f0fb609ec38f95d9bd0ab5dffbae
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-07 18:15:20 +0530

    update function reverseString and reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let newWord = '';
+  for(let i = word.length -1; i >= 0; i--){
+    newWord += word[i]
+  }
+  return newWord
 };
 
 // Do not edit below this line
