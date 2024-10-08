a2958c6922f6ce93003c96688c5531b46cf4c6a7
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-08 15:37:59 +0530

    update removeFromArray to pass the test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementArray) {
+  for(let i = 0; i < arr.length; i++){
+      if(elementArray.includes(arr[i])){
+           arr.splice(i, 1);
+           i--;
+      }
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
